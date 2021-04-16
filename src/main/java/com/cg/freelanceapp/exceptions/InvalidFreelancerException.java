fbbb59511182f080c32b1f9b6accac2ef4b51083
vvr3ddy <vvr-rockstar@bmsit.in>
2021-04-16 15:28:54 +0530

    :octocat: Add Custom Exceptions
    + Added InvalidFreelancerException
    + Added InvalidRecruiterException
    
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

---
+++ src/main/java/com/cg/freelanceapp/exceptions/InvalidFreelancerException.java
@@ -0,0 +1,8 @@
+package com.cg.freelanceapp.exceptions;
+
+public class InvalidFreelancerException extends RuntimeException{
+
+    public InvalidFreelancerException(String msg){
+        super(msg);
+    }
+}
---
+++ src/main/java/com/cg/freelanceapp/exceptions/InvalidRecruiterException.java
@@ -0,0 +1,7 @@
+package com.cg.freelanceapp.exceptions;
+
+public class InvalidRecruiterException extends RuntimeException{
+    public InvalidRecruiterException(String msg){
+        super(msg);
+    }
+}
