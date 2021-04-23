4fe2f4b2e9a38acd07748e4fe6139640d6beea77
vvr3ddy <vvr-rockstar@bmsit.in>
2021-04-23 21:28:15 +0530

    :octocat: :gear: Unit Tests: Create a simple unit test
    + Create a test for skill module
    
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- src/test/java/com/cg/OnlineJobPortalApplicationTests.java
+++ src/test/java/com/cg/OnlineJobPortalApplicationTests.java
@@ -1,13 +1,27 @@
 package com.cg;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.cg.freelanceapp.entities.Skill;
+import com.cg.freelanceapp.service.ISkillService;
 
 @SpringBootTest
 class OnlineJobPortalApplicationTests {
 
+	@Autowired
+	ISkillService skillService;
+	
 	@Test
-	void contextLoads() {
+	void test1() {
+		long id = 7;
+		Skill skill = new Skill(id, "Python", "Python Skill");
+		skillService.save(skill);
+		System.out.println(skillService.findById(id).getName());
+		assertEquals("Python", skillService.findById(id).getName());
 	}
 
 }
